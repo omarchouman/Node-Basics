8c45d571344e3f09d582df50c439e55b03b772fd
Omar Chouman <omar.chouman0@gmail.com>
2021-06-10 11:44:18 +0300

    extended hello

--- tasks.js
+++ tasks.js
@@ -37,8 +37,8 @@ function onDataReceived(text) {
   if (text === 'quit\n' || text === 'exit\n') {
     quit();
   }
-  else if(text === 'hello\n'){
-    hello();
+  else if(text === 'hello\n' || text.startsWith("hello ") || text.startsWith("hello\t")){
+    hello(text);
   }
   else if(text === 'help\n'){
     help()
@@ -66,8 +66,10 @@ function unknownCommand(c){
  *
  * @returns {void}
  */
-function hello(){
-  console.log('hello!')
+function hello(text){
+  text = text.replace("\n", "");
+  text = text.trim(" ");
+  console.log(text);
 }
 
 /**
