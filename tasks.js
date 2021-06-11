f569d0eb669d5a1d4c5f07065fdfff1b6cf0089b
Omar Chouman <omar.chouman0@gmail.com>
2021-06-11 14:43:25 +0300

    configurable save file

--- tasks.js
+++ tasks.js
@@ -9,7 +9,7 @@
  * @param  {string} name the name of the app
  * @returns {void}
  */
-function startApp(name){
+ function startApp(name){
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
   process.stdin.on('data', onDataReceived);
@@ -17,8 +17,6 @@ function startApp(name){
   console.log("--------------------")
 }
 
-
-var list = [['','1'],['✓','2'],['','3']];
 
 /**
  * Decides what to do depending on the data that was received
@@ -35,34 +33,40 @@ var list = [['','1'],['✓','2'],['','3']];
  * @param  {string} text data typed by the user
  * @returns {void}
  */
+// var list = [['','11'],['✓','2'],['','3']];
+var list = [];
+var fs = require('fs');
+var path="./database.json";
+var path="./test.json";
 function onDataReceived(text) {
   if (text === 'quit\n' || text === 'exit\n') {
     quit();
   }
-  else if(text === "hello\n" || text.startsWith("hello") || text.startsWith("hello\t")){
+  else if(text === 'hello\n' || text.startsWith("hello ") || text.startsWith("hello\t")){
     hello(text);
   }
-  else if(text === "help\n"){
-    help()
+  else if(text === 'help\n'){
+    help();
   }
   else if(text.startsWith("list")){
     tasks(list);
   }
-  else if(text.startsWith("add")){
-      add(text);
+  else if(text.startsWith("add ")){
+    add(text);
   }
-  else if(text === "remove\n" || text.startsWith("remove")){
+  else if(text === 'remove\n' ||text.startsWith("remove ")){
     remove(text);
   }
-  else if(text.startsWith("edit")){
+  else if(text.startsWith("edit ")){
     edit(text);
   }
-  else if(text.startsWith("check")){
+  else if(text.startsWith("check ")){
     check(text);
   }
-  else if(text.startsWith("uncheck")){
+  else if(text.startsWith("uncheck ")){
     uncheck(text);
   }
+
   else{
     unknownCommand(text);
   }
@@ -82,97 +86,126 @@ function unknownCommand(c){
 
 
 /**
- * says hello and your name if you type it otherwise it says only hello
+ * Says hello
  *
  * @returns {void}
  */
-function hello(text){
-  text = text.replace("\n", "");
-  text = text.trim(" ");
-  console.log(text);
+function hello(c){
+  console.log(c.trim()+"!!")
 }
 
+
 /**
- * Lists all the possible commands that are valid inside the app
+ * Exits the application
+ *
+ * @returns {void}
+ */
+function quit(){
+  console.log('Quitting now, goodbye!');
+  fs.open(path,'w+',function(err,file){
+    if(err){
+      throw err;
+    } 
+
+    fs.writeFileSync(path,"");
+    fs.appendFileSync(path,"{\n");
+    fs.appendFileSync(path,'\t"list": [\n');
+    for(let i=0;i<list.length;i++){
+      fs.appendFileSync(path,"\t\t{\n");
+      fs.appendFileSync(path,'\t\t\t"name" : "'+list[i][1]+"\",\n");
+      if(list[i][0]==""){
+        fs.appendFileSync(path,'\t\t\t"checked" : false\n');
+      }
+      else {
+        fs.appendFileSync(path,'\t\t\t"checked" : true\n');
+      }
+      if(i==list.length-1){
+        fs.appendFileSync(path,"\t\t}\n");
+      }
+      else{
+        fs.appendFileSync(path,"\t\t},\n");
+      }
+      
+      
+    }
+    fs.appendFileSync(path,'\t]\n');
+    
+    fs.appendFileSync(path,"}\n");
+    fs.close(file,function(){
+      process.exit();
+
+    });
+    
+    
+
+
+  });
+  
+}
+/**
+ * list of the commands
  *
  * @returns {void}
  */
 function help(){
-  console.log("\n");
-  console.log("--- All Commands ---\n");
-  console.log("hello - prints hello and greets you\n");
-  console.log("list - lists all the possible commands that you can use\n");
-  console.log("add - adds new task\n");
+  console.log("\n")
+  console.log("quit or exit - quits the application\n");
+  console.log("hello - prints hello\n");
+  console.log("hello yourname - prints hello + your name\n");
+  console.log("list - prints all the possible commands of this application\n");
   console.log("remove - removes a certain task\n");
-  console.log("edit - edits the tasks");
-  console.log("check - marks the task as done\n");
-  console.log("uncheck - demarks the task so it becomes undone");
-  console.log("exit or quit - quits the application\n");
+  console.log("add taskname - adds a certain task\n");
+  console.log("check tasknumber - marks the specified task as done\n");
+  console.log("uncheck task number - demarks the specified tasks so it is not done yet\n");
 }
 
-/**
- * Lists the user's tasks
- *
- * @returns {void}
- */
 function tasks(list){
-  for(var i = 0; i < list.length; i++){
-    console.log(list[i]);
+  for(let i = 0;i<list.length;i++){
+    if(list[i][0]==""){
+    console.log( [i+1] + ") " + " [] " + list[i][1]);
+  }
+  else
+  console.log( [i+1] + ") " + " [✓] " + list[i][1]);
   }
 }
-
-/**
- * Adds a new task
- *
- * @returns {void}
- */
-function add(text){
-  text = text.trim();
-  list.push(text.substring(4).trim());
+function add(c){
+  c = c.trim();
+  list.push(["",c.substring(4).trim()]);
 }
-
-/**
- * Removes an existing task
- *
- * @returns {void}
- */
-function remove(text){
-  text = text.trim();
-  if(text.length == 6){
+function remove(c){
+  c = c.trim();
+  if(c.length == 6){
     list.pop();
   }
-  else if(text.substring(7) >=list.length || text.substring(7)<0){
-    console.log("task number doesn't exist");
-  }
+  else if(c.substring(7) >list.length || c.substring(7)<=0)
+  console.log("taks number doesn't exist");
   else{
-  list.splice(text.substring(7),1);
+    let v = parseInt(c.substring(7)) - 1;
+  list.splice(v,1);
 }
 }
 
-/**
- * Edits the task
- *
- * @returns {void}
- */
 function edit(c){
   c = c.replace("\n","");
-  c=  c.split(" ");
-
+  c=c.split(" ");
+  console.log(c[1]);
   for(var i = 0;i<c[1].length;i++){
     if(c[1][i]<"0" || c[1][i]>"9"){
-      break;
+    break;
+    }
+    else{
     }
   }
   var str="";
-  if(i==c[1].length){
+  if(i==c[1].length && c.length>2){
     for(let j=2;j<c.length;j++){
       if(j>2)
       str+=" "+c[j];
       else
       str+=c[j]
     }
-    list[i] = str;
-
+    console.log(list[i][1]);
+    list[c[1]-1][1] = str;
   }
   else{
     for(let j=1;j<c.length;j++){
@@ -181,39 +214,44 @@ function edit(c){
       else
       str+=c[j]
     }
-    list[list.length-1] = str;
+    list[list.length-1][1] = str;
   }
 }
-
-/**
- * marks as done
- *
- * @returns {void}
- */
 function check(text){
   text = text.split(" ");
   list[text[1]-1][0] = '✓';
 }
-
-/**
- * demarks the task
- *
- * @returns {void}
- */
 function uncheck(text){
   text = text.split(" ");
   list[text[1]-1][0] = '';
 }
+function create(){
+  var myargs = process.argv.slice(2);
+  if(typeof(myargs[0])!="undefined"){
+    path ="./"+myargs[0];
+  }
+  var config;
+  fs.readFile(path,'utf8',(err,data) => {
+    try{
+    config = JSON.parse(data);
+    for(let i = 0;i<config.list.length;i++){
+      if(config.list[i].checked){
+        list.push(["✓",config.list[i].name]);
+      }
+      else{
+        list.push(["",config.list[i].name]);
+      }
+    
+    }
+  }
+  catch(e){
 
-/**
- * Exits the application
- *
- * @returns {void}
- */
-function quit(){
-  console.log('Quitting now, goodbye!')
-  process.exit();
+  }
+  });
+  
+
+
+  
 }
-
-// The following line starts the application
 startApp("Omar Chouman")
+create();
